5eff20ef530720e55c9508b26641b7a07f2e9108
Dermah <inquiry@dermah.com>
2015-03-18 18:49:57 +1100

    Make node bootstrap take a folder argument

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -1,9 +1,16 @@
-dest=/path/to/local/folder
+if test "$#" -ne 1; then
+	echo "Usage: ./node-bootstrap.sh /path/to/local/folder/for/node"
+	exit
+fi
 
-cd $dest
+mkdir $1
+cd $1
 
 curl -O http://nodejs.org/dist/v0.12.0/node-v0.12.0-darwin-x64.tar.gz
 tar -xvf node-v0.12.0-darwin-x64.tar.gz 
 cd node-v0.12.0-darwin-x64
 
+# does not persist outside of this script
 PATH=$PATH:$(pwd)
+
+npm
