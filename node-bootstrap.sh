f05d6987d4dc5ab6587987bed4bf5b8878f13221
Dermah <inquiry@dermah.com>
2015-05-27 19:38:20 +1000

    Make the bootstrap run at cofa a lot better, plus better reporting

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if test "$#" -ne 2; then
 	echo "Usage: ./node-bootstrap.sh /path/to/working/folder /path/to/listener.js "
 	echo "For example:"
@@ -5,21 +6,26 @@ if test "$#" -ne 2; then
 	exit
 fi
 
+echo "- CREATING SUPERNOVA OBSERVER"
+
 mkdir $1
 cd $1
 
 cp $2 ./
 
-mkdir $1/node
-cd $1/node
+mkdir $1/nodeBin
+cd $1/nodeBin
 
 curl -O http://nodejs.org/dist/v0.12.0/node-v0.12.0-darwin-x64.tar.gz
 tar -xf node-v0.12.0-darwin-x64.tar.gz 
 cd node-v0.12.0-darwin-x64/bin
 
-# does not persist outside of this script
-PATH=$PATH:$(pwd)
-
 cd $1
 
-node $1/listener.js
+ln -s ./nodeBin/node-v0.12.0-darwin-x64/bin/node ./node
+
+echo "- SUCCESS, RUNNING SUPERNOVA OBSERVER"
+
+./node $1/listener.js
+
+echo "- SUPERNOVA OBSERVER DESTROYED"
