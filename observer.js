0e13e162e85b81408c3dae2cdf7f0b2e9057eca0
Sputnik Antolovich <inquiry@dermah.com>
2015-05-31 19:12:51 +1000

    Remove unused copy script

--- observer.js
+++ observer.js
@@ -65,29 +65,4 @@ server.on("message", function (msg, rinfo) {
   }
 });
 
-// stolen from http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
-function copyFile(source, target, cb) {
-  var cbCalled = false;
-
-  var rd = fs.createReadStream(source);
-  rd.on("error", function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(target);
-  wr.on("error", function(err) {
-    done(err);
-  });
-  wr.on("close", function(ex) {
-    done();
-  });
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
-  }
-}
-
 server.bind(6661);
